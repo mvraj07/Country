5440a4466b8bc97fab5bc014c3dbc2f1cd3b69f9
mvraj07 <mvraj7898@gmail.com>
2025-09-06 20:56:00 +0530

    added dashboard

---
+++ src/pages/dashboard.jsx
@@ -0,0 +1,11 @@
+export default function Contact() {
+  return (
+    <div>
+      <h1>Dashboard</h1>
+      <p>
+        Welcome to your dashboard. Here you can view and manage your account
+        information.
+      </p>
+    </div>
+  );
+}
