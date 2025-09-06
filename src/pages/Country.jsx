fd11a3b8a439da2c3d5051207be8fb5d84126657
mvraj07 <mvraj7898@gmail.com>
2025-09-06 20:50:09 +0530

    country page updated

--- src/pages/Country.jsx
+++ src/pages/Country.jsx
@@ -1,3 +1,8 @@
 export default function Contact() {
-  return <></>;
+  return (
+    <div>
+      <h1>Contact Us</h1>
+      <p>If you have any questions, feel free to reach out!</p>
+    </div>
+  );
 }
