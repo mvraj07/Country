446ceaa54e9d038c016a3481c9b63543e1512a8d
mvraj07 <mvraj7898@gmail.com>
2025-09-07 21:53:12 +0530

    updated

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,6 +2,7 @@ export default function Contact() {
   return (
     <div>
       <h1>Contact Us</h1>
+      <p>changes to the home page</p>
       <p>If you have any questions, feel free to reach out!</p>
     </div>
   );
