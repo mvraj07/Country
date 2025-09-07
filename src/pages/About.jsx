0ccfcbd74e31a48f62f2771214bd343c9cb691fa
mvraj07 <mvraj7898@gmail.com>
2025-09-07 21:52:14 +0530

    about

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -3,6 +3,7 @@ export default function Contact() {
     <div>
       <h1>Contact Us</h1>
       <p>If you have any questions, feel free to reach out!</p>
+      <p>thisis about page</p>
     </div>
   );
 }
